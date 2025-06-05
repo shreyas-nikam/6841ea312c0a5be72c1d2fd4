0ad578e4a75af48ac012f4c032997a38ca1607d4
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-06-05 15:18:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from application_pages.page1 import run_page1
+from application_pages.page2 import run_page2
 
-st.set_page_config(page_title="QuCreate Streamlit Lab", layout="wide")
-st.sidebar.image("assets/images/company_logo.jpg")
+st.set_page_config(page_title="Investment Base Pairs Lab", layout="wide")
+st.sidebar.image("https://www.quantuniversity.com/assets/img/logo5.jpg")
 st.sidebar.divider()
-st.title("QuCreate Streamlit Lab")
+st.title("Investment Base Pairs Lab")
 st.divider()
 
-# Code goes here
+st.markdown("This lab explores Investment Base Pairs.")
+
+page = st.sidebar.selectbox(label="Navigation", options=["Data Exploration", "Performance Analysis"])
+
+if page == "Data Exploration":
+    run_page1()
+elif page == "Performance Analysis":
+    run_page2()
 
 st.divider()
 st.write("© 2025 QuantUniversity. All Rights Reserved.")
 st.caption("The purpose of this demonstration is solely for educational use and illustration. "
-           "To access the full legal documentation, please visit this link. Any reproduction of this demonstration "
+           "Any reproduction of this demonstration "
            "requires prior written consent from QuantUniversity.")
